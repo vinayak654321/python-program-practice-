82ff9d80b8a76b0a55e890d7808ad08b14d64d01
Vinayak Bhise <vbhise956@gmail.com>
2026-01-11 09:10:42 +0530

    Added basic operators, input-output and string programs

---
+++ 02_basic_operators.py
@@ -0,0 +1,18 @@
+a = 10
+b = 5
+
+# Arithmetic Operators
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+# Comparison Operators
+print(a > b)
+print(a == b)
+print(a < b)
+
+# Logical Operators
+print(a > 5 and b < 10)
+print(a > 20 or b < 10)
+print(not(a > b))
---
+++ 04_strings.py
@@ -0,0 +1,14 @@
+text = "Python Programming"
+
+# Indexing
+print(text[0])
+print(text[7])
+
+# Slicing
+print(text[0:6])
+print(text[7:])
+
+# String methods
+print(text.upper())
+print(text.lower())
+print(text.replace("Python", "Java"))
